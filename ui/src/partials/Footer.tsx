d2a9e73c523f6ae3348296c0789741d6573c1167
Osaremhen Ukpebor <osas.solo@outlook.com>
2023-05-15 13:43:44 +0100

    Added footer

--- ui/src/partials/Footer.tsx
+++ ui/src/partials/Footer.tsx
@@ -8,18 +8,20 @@ const Footer = () => {
                 <Col className="col-auto text-start p-3">
                     &copy; {new Date().getFullYear()} Osaremhen Solomon Ukpebor
                 </Col>
-                <Col className="col-auto text-end justify-content-end p-2 px-auto">
+                <Col className="col-auto text-end justify-content-end align-items-end p-2 px-auto">
                     <Nav>
                         <NavItem>
                             <NavLink href="https://github.com/Osas-Solo" target="_blank">
                                 GitHub
                             </NavLink>
                         </NavItem>
+                        <div className="vr"/>
                         <NavItem>
                             <NavLink href="https://www.linkedin.com/in/osas-solo/" target="_blank">
                                 LinkedIn
                             </NavLink>
                         </NavItem>
+                        <div className="vr"/>
                         <NavItem>
                             <NavLink href="https://twitter.com/OsasSolo" target="_blank">
                                 Twitter

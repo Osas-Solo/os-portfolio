6fa2365a8b8f88fd08ce7ffa99f4b6417e2b90cd
Osaremhen Ukpebor <osas.solo@outlook.com>
2023-05-16 21:46:31 +0100

    Updated header

--- ui/src/partials/Header.tsx
+++ ui/src/partials/Header.tsx
@@ -1,26 +1,36 @@
-import {Nav, NavItem} from "reactstrap";
+import {Collapse, Nav, Navbar, NavbarToggler, NavItem} from "reactstrap";
 import {Link} from "react-router-dom";
-import React from "react";
+import React, {useState} from "react";
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
     return (
-        <Nav className="p-3 mb-md-5 justify-content-center sticky-md-top sticky-bottom">
-            <NavItem>
-                <Link to="/about" className="nav-link fw-bold">
-                    OS About
-                </Link>
-            </NavItem>
-            <NavItem>
-                <Link to="/projects" className="nav-link fw-bold">
-                    OS Projects
-                </Link>
-            </NavItem>
-            <NavItem>
-                <Link to="/contact" className="nav-link fw-bold">
-                    OS Contact
-                </Link>
-            </NavItem>
-        </Nav>
+        <Navbar className="navbar-expand-lg">
+            <NavbarToggler onClick={toggle} className="col-2"/>
+
+            <Collapse isOpen={isOpen} navbar>
+                <Nav className="col-lg-12 col-10 flex-column flex-md-row fixed-bottom p-3 mb-md-5 justify-content-center sticky-top" navbar>
+                    <NavItem>
+                        <Link to="/about" className="nav-link fw-bold">
+                            OS About
+                        </Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link to="/projects" className="nav-link fw-bold">
+                            OS Projects
+                        </Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link to="/contact" className="nav-link fw-bold">
+                            OS Contact
+                        </Link>
+                    </NavItem>
+                </Nav>
+            </Collapse>
+        </Navbar>
     );
 }
 
